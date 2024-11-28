bca4bc9e2ce60805283ee3bedf53c644941907c4
ferdyodhiambo <oferdinanddev112@gmail.com>
2024-11-28 13:20:05 +0300

    Chore: implement a solution by handling inputs from the command line

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,5 +1,3 @@
-// 1-stdin.js
-
 const readline = require('readline');
 
 // Create an interface for input and output
@@ -11,19 +9,22 @@ const rl = readline.createInterface({
 // Display the welcome message
 console.log('Welcome to Holberton School, what is your name?');
 
+// Flag to check if input is piped
+const isPipedInput = !process.stdin.isTTY;
+
 // Listen for user input
 rl.on('line', (input) => {
-    // Display the user's name without any additional message
+    // Display the user's name
     console.log(`Your name is: ${input}`);
     
     // Close the readline interface after receiving input
     rl.close();
 });
 
-// Handle the close event to display the closing message only for piped input
+// Handle the close event
 rl.on('close', () => {
-    // Check if the input was from a pipe or not
-    if (process.stdin.isTTY) {
+    // Display the closing message only if input was piped
+    if (isPipedInput) {
         console.log('This important software is now closing');
     }
 });
