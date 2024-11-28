cf2d1d02fb5f8daaab74e63b19c4ba4d6779e3ad
ferdyodhiambo <oferdinanddev112@gmail.com>
2024-11-28 13:06:23 +0300

    Chore: setup project dependncies and first file solution

---
+++ 0x05-Node_JS_basic/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 12
+    },
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "quotes": ["error", "single"],
+        "semi": ["error", "always"]
+    }
+};
--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,6 +1,12 @@
 // 0-console.js
+
+/**
+ * Prints a message to the console.
+ * @param {string} message - The message to print.
+ */
 function displayMessage(message) {
     console.log(message);
 }
 
+// Exporting the function using the required format
 module.exports = displayMessage;
