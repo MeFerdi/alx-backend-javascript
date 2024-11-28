7d7ff0851c48e4325e7e390c678ea0e5e9ec2d01
ferdyodhiambo <oferdinanddev112@gmail.com>
2024-11-28 13:15:00 +0300

    Chore: implement a solution by handling inputs from the command line

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,26 +1,16 @@
-// 1-stdin.js
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const readline = require('readline');
-
-// Create an interface for input and output
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-// Display the welcome message
-console.log('Welcome to Holberton School, what is your name?');
-
-// Listen for user input
-rl.on('line', (input) => {
-    // Display the user's name
-    console.log(`Your name is: ${input}`);
-    
-    // Close the readline interface after receiving input
-    rl.close();
-});
-
-// Handle the close event to display the closing message
-rl.on('close', () => {
-    console.log('This important software is now closing');
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
