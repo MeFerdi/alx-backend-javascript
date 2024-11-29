17b50287a228d67830fec4a5c16a5c461699107c
MeFerdi <ferdinandodhis254@gmail.com>
2024-11-29 10:15:33 +0300

    Chore: test

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,36 +1,15 @@
-// 3-payment.test.js
 const sinon = require('sinon');
 const Utils = require('./utils');
+const { expect } = require('chai');
 const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
-    let spy;
+  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
+    const bigBrother = sinon.spy(Utils);
 
-    beforeEach(() => {
-        // Create a spy for the calculateNumber function
-        spy = sinon.spy(Utils, 'calculateNumber');
-    });
-
-    afterEach(() => {
-        // Restore the original method after each test
-        spy.restore();
-    });
-
-    it('should call Utils.calculateNumber with correct arguments', () => {
-        sendPaymentRequestToApi(100, 20);
-        
-        // Validate that calculateNumber was called with 'SUM', 100, and 20
-        sinon.assert.calledWith(spy, 'SUM', 100, 20);
-    });
-
-    it('should log the correct total', () => {
-        const consoleLogSpy = sinon.spy(console, 'log');
-        
-        sendPaymentRequestToApi(100, 20);
-        
-        // Validate that console.log was called with the correct message
-        sinon.assert.calledWith(consoleLogSpy, 'The total is: 120');
-        
-        consoleLogSpy.restore(); // Restore console.log after test
-    });
+    sendPaymentRequestToApi(100, 20);
+    expect(bigBrother.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    expect(bigBrother.calculateNumber.callCount).to.be.equal(1);
+    bigBrother.calculateNumber.restore();
+  });
 });
--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,12 +1,16 @@
-// utils.js
 const Utils = {
-    calculateNumber: function(type, a, b) {
-        if (type === 'SUM') {
-            return a + b;
-        }
-        // You can add more operations here (e.g., SUBTRACT, MULTIPLY, etc.)
-        return 0;
-    }
-};
-
-module.exports = Utils;
+    calculateNumber(type, a, b) {
+      if (type === 'SUM') {
+        return Math.round(a) + Math.round(b);
+      }
+      if (type === 'SUBTRACT') {
+        return Math.round(a) - Math.round(b);
+      }
+      if (type === 'DIVIDE') {
+        return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
+      }
+      return 0;
+    },
+  };
+  
+  module.exports = Utils;
